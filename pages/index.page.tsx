7f6b8e316f9a453271295523b8159fad58aa81f4
nemesmia-gm <52381038+nemesmia-gm@users.noreply.github.com>
2022-04-26 10:34:02 +0200

    Update index.page.tsx

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,7 +1,20 @@
-import type { NextPage } from "next";
+import styled from "styled-components";
+
+const StyledTitle = styled.div<{ color: string }>`
+  height: 200px;
+  background-color: ${(props)=> props.color };
+  color: red;
+  font-size: 50px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 const Home: NextPage = () => {
-  return <div>Hello Takumi!!!</div>;
+  return <StyledTitle>Hello Takumi!!!</StyledTitle>;
 };
 
 export default Home;
+
